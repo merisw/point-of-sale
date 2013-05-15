564b84a385f933d6ca80bed157d32a9030dcf438
Dave Laird <dave@urbanspoon.com>
2013-05-15 16:21:12 -0700

    --amend

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130430150447) do
+ActiveRecord::Schema.define(:version => 20130303083034) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -41,6 +41,5 @@ ActiveRecord::Schema.define(:version => 20130430150447) do
     t.datetime "created_at",                        :null => false
     t.datetime "updated_at",                        :null => false
   end
-
 
 end
